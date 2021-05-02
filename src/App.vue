395d6404d95cfdb25ef986b5240c95a8b464d070
Timenator77 <t.nobbenhuis@innovadis.com>
2021-05-02 15:37:53 +0200

    Lecture 126

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StoredResources from './components/learning-resources/StoredResources';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    StoredResources
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'offical-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {id: 'google', title: 'Google', description: 'Learn to google...', link: 'https://google.com'}
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
